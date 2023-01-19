c4db9d38e49e8729526de01084eb1be842f0d0ca
jkaiser <jayckaiser@gmail.com>
2023-01-18 20:03:14 -0600

    Refactor construct_yaml_map; Add constructor; Rewrite doc strings

--- earthmover/yaml_parser.py
+++ earthmover/yaml_parser.py
@@ -3,7 +3,6 @@ import os
 from yaml import SafeLoader
 
 
-
 class YamlMapping(dict):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -12,23 +11,49 @@ class YamlMapping(dict):
 
 class SafeLineEnvVarLoader(SafeLoader):
     """
-    This allows us to determine the YAML file line number for any element loaded from YAML
-    (very useful for debugging and giving meaningful error messages)
-    (derived from https://stackoverflow.com/a/53647080)
+    Convert the mapping to a YamlMapping in order to store line number internally
+        - Allows us to determine the line number for any element loaded from YAML file
+        - Very useful for debugging and giving meaningful error messages
+        - See https://stackoverflow.com/a/53647080 and https://stackoverflow.com/a/67254800
 
-    Also added env var interpolation based on
-    https://stackoverflow.com/questions/52412297
+    Add environment variable interpolation
+        - See https://stackoverflow.com/questions/52412297
     """
     def construct_mapping(self, node, deep=False):
+        """
+        Add environment variable interpolation to Constructor.construct_mapping()
+
+        :param node:
+        :param deep:
+        :return:
+        """
         mapping = super().construct_mapping(node, deep=deep)
 
-        # expand env vars:
+        #
         for k, v in mapping.items():
             if isinstance(v, str):
                 mapping[k] = os.path.expandvars(v)
 
-        # Convert the mapping to a YamlMapping in order to store line number internally.
-        # Add 1 so line numbering starts at 1
-        mapping = YamlMapping(mapping)
-        mapping.__line__ = node.start_mark.line + 1
         return mapping
+
+    def construct_yaml_map(self, node):
+        """
+        Add line numbers as attribute of pyyaml.Constructor
+        - See https://github.com/yaml/pyyaml
+
+        :param node:
+        :return:
+        """
+        data = YamlMapping()  # Originally `data = {}`
+        data.__line__ = node.start_mark.line + 1  # Start line numbering at 1
+        yield data
+
+        value = self.construct_mapping(node)
+        data.update(value)
+
+
+
+SafeLineEnvVarLoader.add_constructor(
+    'tag:yaml.org,2002:map',
+    SafeLineEnvVarLoader.construct_yaml_map
+)
