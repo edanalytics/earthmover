112d5f9601149ae8515f17d31141b52dbeda91b7
jkaiser <jayckaiser@gmail.com>
2023-01-18 18:22:05 -0600

    Refactor doc-string; add YamlMapping class that inherits from dict by adding a __line__ attribute (instead of using an extra key).

--- earthmover/yaml_parser.py
+++ earthmover/yaml_parser.py
@@ -3,13 +3,22 @@ import os
 from yaml import SafeLoader
 
 
-# This allows us to determine the YAML file line number for any element loaded from YAML
-# (very useful for debugging and giving meaningful error messages)
-# (derived from https://stackoverflow.com/a/53647080)
-# Also added env var interpolation based on
-# https://stackoverflow.com/questions/52412297/how-to-replace-environment-variable-value-in-yaml-file-to-be-parsed-using-python#answer-55301129
-class SafeLineEnvVarLoader(SafeLoader):
 
+class YamlMapping(dict):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.__line__ = None
+
+
+class SafeLineEnvVarLoader(SafeLoader):
+    """
+    This allows us to determine the YAML file line number for any element loaded from YAML
+    (very useful for debugging and giving meaningful error messages)
+    (derived from https://stackoverflow.com/a/53647080)
+
+    Also added env var interpolation based on
+    https://stackoverflow.com/questions/52412297
+    """
     def construct_mapping(self, node, deep=False):
         mapping = super().construct_mapping(node, deep=deep)
 
@@ -18,6 +27,8 @@ class SafeLineEnvVarLoader(SafeLoader):
             if isinstance(v, str):
                 mapping[k] = os.path.expandvars(v)
 
+        # Convert the mapping to a YamlMapping in order to store line number internally.
         # Add 1 so line numbering starts at 1
-        mapping['__line__'] = node.start_mark.line + 1
+        mapping = YamlMapping(mapping)
+        mapping.__line__ = node.start_mark.line + 1
         return mapping
