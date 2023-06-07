ea3d7ae2c74f5997adf6ad7f729f596982f4bf9f
Tom Reitz <treitz@edanalytics.org>
2023-06-07 13:32:25 -0500

    skip hashing missing optional source files

--- earthmover/runs_file.py
+++ earthmover/runs_file.py
@@ -158,7 +158,7 @@ class RunsFile:
             if f"$sources.{source.name}" not in node_data.keys():
                 continue
 
-            if not source.is_remote:
+            if not source.is_remote and not (source.optional and source.file==''):
                 sources_hash += self._get_file_hash(source.file)
 
         if sources_hash:
