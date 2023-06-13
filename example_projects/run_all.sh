add60dc2469dc1cda8cc6abe0fbb2a7348dcf2b0
Tom Reitz <treitz@edanalytics.org>
2023-06-13 09:24:45 -0500

    update test suite to force regenerate data

--- example_projects/run_all.sh
+++ example_projects/run_all.sh
@@ -4,61 +4,61 @@ echo "running all examples..."
 
 echo "  running 01_simple..."
 cd 01_simple/
-earthmover
+earthmover -f
 rm -f output/*
 echo "  ... done!"
 
 echo "  running 02_join..."
 cd ../02_join/
-earthmover
+earthmover -f
 rm -f output/*
 echo "  ... done!"
 
 echo "  running 03_groupby..."
 cd ../03_groupby/
-earthmover
+earthmover -f
 rm -f output/*
 echo "  ... done!"
 
 echo "  running 04_sqlalchemy..."
 cd ../04_sqlalchemy/
-earthmover
+earthmover -f
 rm -f output/*
 echo "  ... done!"
 
 echo "  running 05_ftp..."
 cd ../05_ftp/
-earthmover
+earthmover -f
 rm -f output/*
 echo "  ... done!"
 
 echo "  running 06_subtemplates..."
 cd ../06_subtemplates/
-earthmover
+earthmover -f
 rm -f output/*
 echo "  ... done!"
 
 echo "  running 07_filetypes..."
 cd ../07_filetypes/
-earthmover
+earthmover -f
 rm -rf output/*
 echo "  ... done!"
 
 echo "  running 08_html..."
 cd ../08_html/
-earthmover
+earthmover -f
 rm -f output/*
 echo "  ... done!"
 
 echo "  running 09_edfi..."
 cd ../09_edfi/
-earthmover
+earthmover -f
 rm -f output/*
 echo "  ... done!"
 
 echo "  running 10_jinja..."
 cd ../10_jinja/
-earthmover
+earthmover -f
 rm -rf outputs/*
 echo "  ... done!"
 
