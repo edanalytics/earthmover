f12fe748adffee6ff82d05c68c125ecf96d441f9
jkaiser <jayckaiser@gmail.com>
2023-08-17 16:08:35 -0500

    Fix Graph.ref() with latest update in rc/1.0.0__yaml_version_2.

--- earthmover/graph.py
+++ earthmover/graph.py
@@ -49,11 +49,12 @@ class Graph(nx.DiGraph, LoggingMixin):
         :param ref:
         :return:
         """
-        _node = self.nodes.get(ref)
-        if _node:
-            return _node['data']
-        else:
-            return None
+        node = self.nodes.get(ref)
+        if not node:
+            raise KeyError(
+                f"Node not found in the graph: {ref}"
+            )
+        return node['data']
 
 
     def select_subgraph(self, selector: str) -> 'Graph':
