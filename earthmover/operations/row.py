fcb6f6a302ddd982b81f1d79af0e544d249ca2d5
jkaiser <jayckaiser@gmail.com>
2023-09-07 15:43:09 -0500

    Update DistinctRowsOperation to index on the column with the most unique values, instead of the first column.

--- earthmover/operations/row.py
+++ earthmover/operations/row.py
@@ -1,3 +1,5 @@
+import dask
+
 from earthmover.nodes.operation import Operation
 
 from typing import List, Tuple
@@ -53,22 +55,36 @@ class DistinctRowsOperation(Operation):
         if not self.columns_list:
             self.columns_list = data.columns
 
-        if data.npartitions > 1:
+        # There are multiple deduping approaches in order to limit memory-usage.
+        if data.npartitions == 1 or len(self.columns_list) == 1:
+            data = data.drop_duplicates(subset=self.columns_list)
+
+        else:
             self.logger.debug(
-                f"data at {self.type} `{self.name}` has {data.npartitions} partitions... indexing on first uniqueness column `{self.columns_list[0]}`, then each partition will be deduped"
+                f"data at {self.type} `{self.name}` has {data.npartitions} partitions..."
+            )
+
+            # Choose the column with the most distinct values to index on before map-partitioning.
+            approx_col_counts = {}
+            for col in self.columns_list:
+                approx_col_counts[col] = data[col].nunique_approx()
+
+            approx_col_counts = dask.compute(approx_col_counts)[0]
+            max_nunique_col = max(approx_col_counts, key=approx_col_counts.get)
+            max_nunique_col_count = int(approx_col_counts[max_nunique_col])
+
+            self.logger.debug(
+                f"indexing on column with the most approximate unique values `{max_nunique_col}` ({max_nunique_col_count}), then each partition will be deduped"
             )
 
             # see https://stackoverflow.com/questions/68019990
             data = (
                 data
-                .set_index(self.columns_list[0], drop=False).repartition(partition_size=self.chunksize)
+                .set_index(max_nunique_col, drop=False).repartition(partition_size=self.chunksize)
                 .map_partitions(lambda x: x.drop_duplicates(self.columns_list))
             )
 
-        else:
-            data = data.drop_duplicates(subset=self.columns_list)
-
-        return data
+        return data.repartition(partition_size=self.chunksize)
 
 
 class FilterRowsOperation(Operation):
