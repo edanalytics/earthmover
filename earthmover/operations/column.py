943a4d3895cb79cb09807a7f12757497f185b5d5
Angelica Lastra <angelicaalastra@gmail.com>
2024-01-25 14:09:38 -0800

    fixed error

--- earthmover/operations/column.py
+++ earthmover/operations/column.py
@@ -492,7 +492,7 @@ class DateFormatOperation(Operation):
         for _column in self.columns_list:
             try:
                 data[_column] = (
-                    pd.dataframe.to_datetime(data[_column]).mask(data[_column].notnull(), 
+                    pd.to_datetime(data[_column]).mask(data[_column].notnull(), 
                        dask.dataframe.to_datetime(data[_column], format=self.from_format)
                        .dt.strftime(self.to_format)))
                 
