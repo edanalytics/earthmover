50c172ac762bcbcc97ea502cbab7058e6777a9f0
Angelica Lastra <angelicaalastra@gmail.com>
2024-01-25 11:50:56 -0800

    fixed error

--- earthmover/operations/column.py
+++ earthmover/operations/column.py
@@ -494,8 +494,7 @@ class DateFormatOperation(Operation):
             try:
                 mask = data[_column].notnull()
                 data = data.assign(**{_column: dask.dataframe.to_datetime(data.loc[mask, _column], format=self.from_format)
-                    .dt.strftime(self.to_format)}
-                )
+                                      .dt.strftime(self.to_format)})
 
             except Exception as err:
                 self.error_handler.throw(
