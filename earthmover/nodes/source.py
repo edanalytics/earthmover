831d4f8e6c09ccecf6136221cb1bec602b648ba2
jayckaiser <jayckaiser@gmail.com>
2024-02-28 15:15:02 -0600

    Remove force-fillna on sources.

--- earthmover/nodes/source.py
+++ earthmover/nodes/source.py
@@ -82,7 +82,7 @@ class Source(Node):
             )
             self.data = dd.from_pandas(self.data, chunksize=self.chunksize)
 
-        self.data = self.opt_repartition(self.data).fillna('')  # Repartition if specified; fill NAs with empty strings.
+        self.data = self.opt_repartition(self.data)  # Repartition if specified.
 
         # Add missing columns if defined under `optional_fields`.
         if self.optional_fields:
