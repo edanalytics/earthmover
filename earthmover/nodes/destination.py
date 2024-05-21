310a565ac0a3c745da960f3b446674157aded13d
keenzarate213 <kzarate@edanalytics.org>
2024-05-21 15:11:04 -0500

    remove extra param in init

--- earthmover/nodes/destination.py
+++ earthmover/nodes/destination.py
@@ -156,7 +156,6 @@ class CsvDestination(Destination):
         self.limit = self.error_handler.assert_get_key(self.config, 'limit', dtype=int, required=False, default=None)
         self.extension = self.error_handler.assert_get_key(self.config, 'extension', dtype=str, required=False, default="csv")
         self.keep_columns = self.error_handler.assert_get_key(self.config, 'keep_columns', required=False, default=None)
-        self.config.get('keep_columns', None)
         
         self.file = os.path.join(
             self.earthmover.state_configs['output_dir'],
