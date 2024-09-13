58e9dc0d16ba9d2356992d281cb6370635e7684b
johncmerfeld <John.Merfeld@gmail.com>
2024-09-13 09:17:10 -0500

    add comment explaining whythe optimizer needs to be turned off

--- earthmover/__init__.py
+++ earthmover/__init__.py
@@ -1,2 +1,7 @@
+# September 2024 - for now we need to do this in order to turn off the Dask 
+#    query optimizer - see https://blog.dask.org/2023/08/25/dask-expr-introduction
+#    For reasons unknown, it doesn't yet work with Earthmover. A future Dask 
+#    version may force us to use the query optimizer, but hopefully by then,
+#    the bugs that emerge when we use it with Earthmover will have been fixed.
 import dask
 dask.config.set({'dataframe.query-planning': False})
