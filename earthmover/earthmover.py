f7c2bf69cfd4018dda2096a36e10b5b54222a628
johncmerfeld <John.Merfeld@gmail.com>
2024-10-28 16:23:54 -0500

    be smarter about results file

--- earthmover/earthmover.py
+++ earthmover/earthmover.py
@@ -63,7 +63,8 @@ class Earthmover:
         self.force = force
         self.skip_hashing = skip_hashing
 
-        self.results_file = os.path.abspath(results_file)
+        self.results_file = os.path.abspath(results_file) if results_file else None
+        print(self.results_file)
         self.config_file = os.path.abspath(config_file)
         self.overrides = overrides
         self.compiled_yaml_file = COMPILED_YAML_FILE
